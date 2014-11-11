9a402afb7b8e8d21b2a928ff2d608ab3e01ce33e
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-11-11 11:49:31 -0500

    'Nother commit

--- lib/guide.rb
+++ lib/guide.rb
@@ -76,12 +76,29 @@ class Guide
     when 'find'
       puts "Finding..."
     when 'add'
-      puts "Adding..."
+      add
     when 'quit'
       return :quit
     else
       puts "\nI don't understand that command.\n"
     end
+  end
+
+  def add
+    puts "\nAdd a restaurant\n\n".upcase
+    restaurant = Restaurant.new
+    print "Restaurant name: "
+    restaurant.name = gets.chomp.strip
+    print "Cuisine type: "
+    restaurant.cuisine = gets.chomp.strip
+    print "Average price: "
+    restaurant.price = gets.chomp.strip
+
+    if restaurant.save
+      puts "\nRestaurant Added\n\n"
+    else
+      puts "\nSave Error: Restaurant not added\n\n"
+    end # An instance method on the Restaurant class that we'll save
   end
 
   def intro
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -5,6 +5,7 @@ class Restaurant
   def self.filepath=(path=nil)
   	@@filepath = File.join(APP_ROOT, path)
   end
+  attr_accessor :name, :cuisine, :price
 
   def self.file_exists?
   	# class should know if a restaurant file file_exists
@@ -29,10 +30,20 @@ class Restaurant
     return file_usable?
   end      
     
-  end
+  
 
   def self.saved_restaurants
   	# read the restaurant file
   	# return instances of restaurant
   end
+
+  def save
+    return false unless Restaurant.file_usable?
+    File.open(@@filepath, 'a') do |file|
+      file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
+    end
+    return true
+  end
+end
+  
 
